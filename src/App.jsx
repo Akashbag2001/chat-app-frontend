9c084adc5c8dff8f3bda3b6010d67327df33f668
AkashBag2004 <akash.bag@iemlabs.com>
2025-02-10 18:20:08 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+import HomePage from "./pages/HomePage"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
