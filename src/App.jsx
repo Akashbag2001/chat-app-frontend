fe0f4d0a5b157e8e53f00eaa0e604809fa86e390
AkashBag2004 <akash.bag@iemlabs.com>
2025-02-12 18:19:45 +0530

    sdasas

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,26 @@ import LoginPage from "./pages/LoginPage"
 import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import HomePage from "./pages/HomePage"
-
+import { useAuthStore } from "./store/useAuthStore"
+import { useEffect } from "react"
+import {Loader} from "lucide-react"
 function App() {
+  const { authUser, checkAuth,isCheckingAuth } = useAuthStore();
 
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log(authUser)
+
+  if(isCheckingAuth && !authUser ){
+    return(
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="animate-spin size-10"/>
+      </div>
+    )
+
+  }
 
   return (
     <>
